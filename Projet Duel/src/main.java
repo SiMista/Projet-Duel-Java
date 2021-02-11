6666848fb5d529c9467f51aa6dc0a165d2225293
SiMista <78701925+SiMista@users.noreply.github.com>
2021-02-11 18:04:14 +0100

    Update main.java

--- Projet Duel/src/main.java
+++ Projet Duel/src/main.java
@@ -4,8 +4,6 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("igsjdqhgshjdgfhjsgf");
- c toi le fdp
- FACTION <3; c'est votre bde ?
 	}
 
 }
