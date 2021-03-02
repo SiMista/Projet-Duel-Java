7a10fa07d2a152c5413c6d852b35121f9764a6e8
Jules DE ALMEIDA <jules.de-almeida@etu.u-paris.fr>
2021-03-02 23:16:44 +0100

    Déplacer les fonctions de scan
    J'voulais faire plus mais je suis troooop claqué, faut qu'je dorme les autres jours je serai plus productif

--- Projet Duel/src/duel/Joueur.java
+++ Projet Duel/src/duel/Joueur.java
@@ -37,36 +37,6 @@ public class Joueur {
 	}
 
 
-	public void jouerCartes() {
-		Scanner sc = new Scanner(System.in);
-		String s;
-		System.out.print("> ");
-		s = sc.nextLine();
-		while (!s.equals("fin")) {
-			décompose(s);
-			System.out.print("> ");
-			s = sc.nextLine();
-		}
-	}
-
-	public void décompose(String s) {
-		// une solution
-		String[] tab = s.split("\\s+");
-		for (String mot : tab)
-			System.out.println(mot);
-
-		// une autre solution
-		Scanner scs = new Scanner(s);
-		while (scs.hasNext()) {
-			String mot = scs.next();
-			if (Character.isDigit(mot.charAt(0))) {
-				int n = Integer.parseInt("" + mot.charAt(0));
-				System.out.println("'" + mot + "' commence par " + n);
-			} else
-				System.out.println("'" + mot + "' ne commence pas par un chiffre");
-		}
-		scs.close();
-	}
 
 	public boolean gagnerPartie() {
 		boolean b = false;
--- Projet Duel/src/duel/Règles.java
+++ Projet Duel/src/duel/Règles.java
@@ -1,5 +1,47 @@
 package duel;
 
+<<<<<<< Updated upstream
 public class Règles {
 	
+=======
+import java.util.Scanner;
+
+public class Règles {
+	private boolean tourDeNord = true;
+	
+	public void jouerUnCoup(Joueur JoueurActif, Joueur JoueurPassif) {
+		
+	}
+	
+	public void jouerCartes() {
+		Scanner sc = new Scanner(System.in);
+		String s;
+		System.out.print("> ");
+		s = sc.nextLine();
+		while (!s.equals("fin")) {
+			décompose(s);
+			System.out.print("> ");
+			s = sc.nextLine();
+		}
+	}
+
+	public void décompose(String s) {
+		/* une solution
+		String[] tab = s.split("\\s+");
+		for (String mot : tab)
+			System.out.println(mot);
+*/
+		// une autre solution
+		Scanner scs = new Scanner(s);
+		while (scs.hasNext()) {
+			String mot = scs.next();
+			if (Character.isDigit(mot.charAt(0))) {
+				int n = Integer.parseInt("" + mot.charAt(0));
+				System.out.println("'" + mot + "' commence par " + n);
+			} else
+				System.out.println("'" + mot + "' ne commence pas par un chiffre");
+		}
+		scs.close();
+	}
+>>>>>>> Stashed changes
 }
