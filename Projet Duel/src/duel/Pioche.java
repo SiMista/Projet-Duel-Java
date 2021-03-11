ab43cac063f390aac0fbbd94b45c0ce9d5351fc9
SiMista <78701925+SiMista@users.noreply.github.com>
2021-03-11 23:37:44 +0100

    On a oublié des maj

--- Projet Duel/src/duel/Pioche.java
+++ Projet Duel/src/duel/Pioche.java
@@ -5,22 +5,22 @@ import java.util.Collections;
 
 public class Pioche {
 
-	private ArrayList<Integer> piocheliste; 
+	private ArrayList<Integer> piocheListe; 
 	private int taille;
 
 	public Pioche() {
 		taille = 58;
-		piocheliste = new ArrayList<Integer>();
+		piocheListe = new ArrayList<Integer>();
 		for (int i = 0; i < taille + 2; i++) {
 			if (i > 1)
-				piocheliste.add(i);
+				piocheListe.add(i);
 		}
-		Collections.shuffle(piocheliste);
+		Collections.shuffle(piocheListe);
 	}
 	
 	public int getCarte() { 
 		taille--;
-		return piocheliste.remove(0);
+		return piocheListe.remove(0);
 	}
 	
 	public int getTaille() {
