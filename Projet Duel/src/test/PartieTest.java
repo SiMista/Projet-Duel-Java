534f17cd504bd9930a90cab9a567cd4ddc0799dd
Jules DE ALMEIDA <jules.de-almeida@etu.u-paris.fr>
2021-03-12 23:17:06 +0100

    Create PartieTest.java

---
+++ Projet Duel/src/test/PartieTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PartieTest {
+
+	@Test
+	void testDécomposeCartes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testConditionVictoire() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPartieFinie() {
+		fail("Not yet implemented");
+	}
+
+}
